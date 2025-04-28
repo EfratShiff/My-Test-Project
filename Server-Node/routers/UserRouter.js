33d32acae9b03b2c11045950891a26aacce4182a
EfratShiff <e0556721339@gmail.com>
2025-04-28 21:31:09 +0300

    check

--- Server-Node/routers/UserRouter.js
+++ Server-Node/routers/UserRouter.js
@@ -1,10 +1,30 @@
 const express = require("express")
 const router = express.Router()
-
 const {createUser,deleteUser}= require("../controllers/UserController")
+const UserAuthentication = require('../middleware/UserAuthentication');
 
-router.post("/createUser",createUser)
-router.delete('/deleteUser/:name/:passwordUser', deleteUser);
+
+router.post("/createUser",/*UserAuthentication,*/createUser)
+// router.delete('/deleteUser/:name/:passwordUser', deleteUser);
+
+router.delete('/deleteUser', deleteUser);
 
 
 module.exports= router
+
+
+
+
+// // routers/UserRouter.js
+// const express = require("express");
+// const router = express.Router();
+// const { createUser, deleteUser } = require("../controllers/UserController");
+// const UserAuthentication = require('../middleware/UserAuthentication');
+
+// // ניתוב ליצירת משתמש חדש
+// router.post("/createUser", UserAuthentication, createUser);
+
+// // ניתוב למחיקת משתמש
+// router.delete('/deleteUser', deleteUser);
+
+// module.exports = router;
